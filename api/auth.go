68fbe076d70fd18f575eab6dc9597eb6a09bad87
Lucas Servén Marín <lserven@gmail.com>
2019-03-22 00:41:08 +0100

    api: remove unused auth code

--- api/auth.go
+++ api/auth.go
@@ -1,9 +1,7 @@
 package api
 
 import (
-	"errors"
 	"math/rand"
-	"net/http"
 
 	"github.com/dghubble/sessions"
 )
@@ -15,13 +13,8 @@ const (
 	sessionSheetKey = "sheetID"
 )
 
-// Authentication Errors
 var (
-	// ErrInvalidState represents a non-matching or empty state parameter.
-	ErrInvalidState = errors.New("oauth2: Invalid OAuth2 state parameter")
-	// ErrMissingStateCode represents a missing state or code in the callback request.
-	ErrMissingStateCode = errors.New("oauth2: Request missing code or state")
-	sessionStore        = sessions.NewCookieStore(randBytes(32), nil)
+	sessionStore = sessions.NewCookieStore(randBytes(32), nil)
 )
 
 func randBytes(n int) []byte {
@@ -30,19 +23,4 @@ func randBytes(n int) []byte {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return b
-}
-
-// parseCallback parses the "code" and "state" parameters from the http.Request
-// and returns them.
-func parseCallback(req *http.Request) (authCode, state string, err error) {
-	err = req.ParseForm()
-	if err != nil {
-		return "", "", err
-	}
-	authCode = req.Form.Get("code")
-	state = req.Form.Get("state")
-	if authCode == "" || state == "" {
-		return "", "", ErrMissingStateCode
-	}
-	return authCode, state, nil
 }
